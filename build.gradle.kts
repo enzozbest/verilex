a69034255e28eae7b7841de7a8452390b30ba919
Enzo Zuanazi Bestetti <enzobestetti3@gmail.com>
2026-04-01 15:48:22 +0100

    Add auto-generated lexer from formal proof

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,8 @@ repositories {
 }
 
 dependencies {
+    implementation(files("libs/verified-lexer.jar"))
+    implementation("org.scala-lang:scala3-library_3:3.3.4")
     testImplementation(kotlin("test"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
 }
