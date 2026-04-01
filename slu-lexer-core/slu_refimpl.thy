0b1b8d461d112aa7d1b763c7f4cb7fb7346c4ed5
Enzo Zuanazi Bestetti <enzobestetti3@gmail.com>
2026-04-01 15:51:49 +0100

    Remove failed extraction attempt

--- slu-lexer-core/slu_refimpl.thy
+++
@@ -1,9 +0,0 @@
-theory slu_refimpl 
-  imports "Posix-Lexing.Lexer3"
-begin
-
-export_code lexer in Scala
-  module_name "slu_refimpl"
-  file_prefix "generated/"
-
-end                                                                          
